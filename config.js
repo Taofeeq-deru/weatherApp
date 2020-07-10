2b0b38c6f3c526967ae37b5401649fa45bf6d33f
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-07-10 15:13:23 +0100

    better

---
+++ config.js
@@ -0,0 +1,7 @@
+// const googleApi = `AIzaSyDDVxlDxBMFkM37SKcxczPnUUuSNr7qwp4`;
+// const openWeatherApi = `2c52f44abc37225f70c03a60124e1680`;
+
+const config = {
+  googleApi: "AIzaSyDDVxlDxBMFkM37SKcxczPnUUuSNr7qwp4",
+  openWeatherApi: "2c52f44abc37225f70c03a60124e1680",
+};
